4fc723549fb413b7ca6f5b57ee4dcaf11e7f5049
deguchi0527 <ryodegu0527@gmail.com>
2021-04-29 11:43:40 +0900

    テーブル/モデルの削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_26_063422) do
+ActiveRecord::Schema.define(version: 2021_04_25_075644) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -33,21 +33,14 @@ ActiveRecord::Schema.define(version: 2021_04_26_063422) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
-  create_table "tweet_links", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.text "link_one"
-    t.text "link_two"
-    t.text "link_three"
-    t.text "link_four"
-    t.bigint "tweet_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["tweet_id"], name: "index_tweet_links_on_tweet_id"
-  end
-
   create_table "tweets", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title", null: false
     t.text "info", null: false
     t.integer "category_id", null: false
+    t.text "link_one"
+    t.text "link_two"
+    t.text "link_three"
+    t.text "link_four"
     t.bigint "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
@@ -69,6 +62,5 @@ ActiveRecord::Schema.define(version: 2021_04_26_063422) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "tweet_links", "tweets"
   add_foreign_key "tweets", "users"
 end
