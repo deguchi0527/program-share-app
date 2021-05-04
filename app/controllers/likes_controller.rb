dfd096d506ff69c04debb914561d38697bc32ba8
deguchi0527 <ryodegu0527@gmail.com>
2021-05-04 15:06:29 +0900

    いいね機能の実装

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -5,7 +5,7 @@ class LikesController < ApplicationController
   end
 
   def destroy
-    @like = Like.find(tweet_id: params[:tweet_id], user_id: current_user.id)
+    @like = Like.find_by(tweet_id: params[:tweet_id], user_id: current_user.id)
     @like.destroy
     redirect_to root_path
   end
