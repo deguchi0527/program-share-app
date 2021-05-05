d3f00807a8e28d637a2ff2dbb045facffd1e0c8d
deguchi0527 <ryodegu0527@gmail.com>
2021-05-05 12:45:37 +0900

    モデル単体テストの実施

--- spec/factories/likes.rb
+++ spec/factories/likes.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :like do
-    
+    association :user
+    association :tweet
   end
 end
--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @like = FactoryBot.build(:like)
+  end
+
+  describe 'いいねの保存' do
+    context 'いいねが保存できる場合' do
+      it 'userとtweetが紐付いていれば保存できる' do
+        expect(@like).to be_valid
+      end
+    end
+    context 'いいねが保存できない場合' do
+      it 'userが紐付いていないと保存できない' do
+        @like.user = nil
+        @like.valid?
+        expect(@like.errors.full_messages).to include('User must exist')
+      end
+      it 'tweetが紐付いていないと保存できない' do
+        @like.tweet = nil
+        @like.valid?
+        expect(@like.errors.full_messages).to include('Tweet must exist')
+      end
+    end
+  end
 end
