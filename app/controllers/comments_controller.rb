5fe5dcfe01f2b00e4cb162bfe87124e318b11753
deguchi0527 <ryodegu0527@gmail.com>
2021-05-06 12:02:52 +0900

    コントローラーの設定

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,7 @@
 class CommentsController < ApplicationController
+  before_action :set_comment, only: :destroy
+  before_action :move_to_show, only: :destroy
+  
   def create
     @comment = Comment.new(comment_params)
     if @comment.save
@@ -9,8 +12,6 @@ class CommentsController < ApplicationController
   end
 
   def destroy
-    @tweet = Tweet.find(params[:tweet_id])
-    @comment = @tweet.comments.find(params[:id])
     if @comment.destroy
       ActionCable.server.broadcast 'delete_channel', comment: @comment
     else
@@ -22,5 +23,14 @@ class CommentsController < ApplicationController
 
   def comment_params
     params.require(:comment).permit(:text).merge(user_id: current_user.id, tweet_id: params[:tweet_id])
+  end
+
+  def set_comment
+    @tweet = Tweet.find(params[:tweet_id])
+    @comment = @tweet.comments.find(params[:id])
+  end
+  
+  def move_to_show
+    redirect_to tweet_path(@comment.tweet.id) if current_user.id != @comment.user_id
   end
 end
